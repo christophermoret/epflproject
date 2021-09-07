6b6ccda614ccb3e0aca9bb08694406051e9dcb2a
Chris <christophermoret@gmail.com>
2021-09-07 15:18:43 +0200

    Level loader ok

--- static/game.js
+++ static/game.js
@@ -3,7 +3,7 @@ const stats = document.getElementById("stats");
 const enemy = document.getElementById("enemy");
 const enemyStats = document.getElementById("enemyStats");
 const actions = document.getElementById("actions");
-const level = localStorage.getItem("Level");
+var level = localStorage.getItem("Level");
 
 // MODELS:
 let player;
@@ -22,43 +22,29 @@ function Monster(name, HP, ATK) {
 
 //Functions
 
-// Level
-function level1() {
-    monster = new Monster("Door guardian", 40, 10);
-    enemy.innerHTML = '<h2>Enemy stats:</h2> <img src="/static/img/guardian.png" style="width:200px;height:200px;">'
-    enemyStats.innerHTML = "<h3>" + monster.name + "</h3>"
-        + "<h3> HP: " + monster.HP + "</h3>"
-        + "<h3>" + "ATK: " + monster.ATK + "</h3>";
-}
-
-function level2() {
-    alert("YOU WON! Moving on to the next level");
-    monster = new Monster("Big BOSS", 500, 500);
-    enemy.innerHTML = '<h2>Enemy stats:</h2> <img src="/static/img/elf.png" style="width:200px;height:200px;">'
-    enemyStats.innerHTML = "<h3>" + monster.name + "</h3>"
-        + "<h3> HP: " + monster.HP + "</h3>"
-        + "<h3>" + "ATK: " + monster.ATK + "</h3>";
-}
-
-
 //Load level
-function loadLevel(level) {
-    console.log("LoadLevel runs");
-    console.log(level);
-
-    if (level = 1) {
+function loadLevel() {
+    if (level == 1) {
         monster = new Monster("Door guardian", 40, 10);
         enemy.innerHTML = '<h2>Enemy stats:</h2> <img src="/static/img/guardian.png" style="width:200px;height:200px;">'
         enemyStats.innerHTML = "<h3>" + monster.name + "</h3>"
             + "<h3> HP: " + monster.HP + "</h3>"
-            + "<h3>" + "ATK: " + monster.ATK + "</h3>";
+            + "<h3> ATK: " + monster.ATK + "</h3>";
     }
-    if (level = 2) {
+    if (level == 2) {
+        monster = new Monster("Goblin", 50, 20);
+        enemy.innerHTML = '<h2>Enemy stats:</h2> <img src="/static/img/goblin.png" style="width:200px;height:200px;">'
+        enemyStats.innerHTML = "<h3>" + monster.name + "</h3>"
+            + "<h3> HP: " + monster.HP + "</h3>"
+            + "<h3> ATK: " + monster.ATK + "</h3>";
+    }
+
+    if (level == 3) {
         monster = new Monster("Big BOSS", 500, 500);
         enemy.innerHTML = '<h2>Enemy stats:</h2> <img src="/static/img/elf.png" style="width:200px;height:200px;">'
         enemyStats.innerHTML = "<h3>" + monster.name + "</h3>"
             + "<h3> HP: " + monster.HP + "</h3>"
-            + "<h3>" + "ATK: " + monster.ATK + "</h3>";
+            + "<h3> ATK: " + monster.ATK + "</h3>";
     }
 }
 
@@ -68,16 +54,19 @@ function attack() {
     monster.HP = monster.HP - player.ATK;
     stats.innerHTML = "<h2>Stats: </h2>" + "<h3>" + player.name + "</h3>"
         + "<h3> HP: " + player.HP + "</h3>"
-        + "<h3>" + "ATK: " + player.ATK + "</h3>"
+        + "<h3> ATK: " + player.ATK + "</h3>"
     enemyStats.innerHTML = "<h3>" + monster.name + "</h3>"
         + "<h3> HP: " + monster.HP + "</h3>"
-        + "<h3>" + "ATK: " + monster.ATK + "</h3>";
+        + "<h3> ATK: " + monster.ATK + "</h3>";
     if (player.HP <= 0) {
         alert("YOU ARE DEAD!")
+        localStorage.setItem("Level", 1);
         document.location.href = 'http://127.0.0.1:5000/hero';
     }
     if (monster.HP <= 0) {
-        level = level + 1;
+        console.log("Ding Dong the witch is dead");
+        localStorage.setItem("Level", ++level);
+        console.log(level);
         loadLevel();
     }
 }
@@ -96,9 +85,4 @@ if (localStorage.getItem("Hero") == "Warrior") {
         + "<h3>" + "ATK: " + player.ATK + "</h3>"
 }
 
-if (localStorage.getItem("Level") == null) {
-    localStorage.setItem("Level", 1);
-}
-
 loadLevel();
-console.log(monster);
