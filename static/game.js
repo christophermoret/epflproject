f8ff2b0c703e8f51b3d7d08cc6b3433632479c3a
Chris <christophermoret@gmail.com>
2021-09-07 15:24:23 +0200

    Clean and bugfixing

--- static/game.js
+++ static/game.js
@@ -25,6 +25,7 @@ function Monster(name, HP, ATK) {
 //Load level
 function loadLevel() {
     if (level == 1) {
+        alert("Welcome to level one, the guardian heard you step in, EN GUARDE!!!!");
         monster = new Monster("Door guardian", 40, 10);
         enemy.innerHTML = '<h2>Enemy stats:</h2> <img src="/static/img/guardian.png" style="width:200px;height:200px;">'
         enemyStats.innerHTML = "<h3>" + monster.name + "</h3>"
@@ -32,6 +33,7 @@ function loadLevel() {
             + "<h3> ATK: " + monster.ATK + "</h3>";
     }
     if (level == 2) {
+        alert("He was no match for you, walking down the stairs to level two, a Goblin appears");
         monster = new Monster("Goblin", 50, 20);
         enemy.innerHTML = '<h2>Enemy stats:</h2> <img src="/static/img/goblin.png" style="width:200px;height:200px;">'
         enemyStats.innerHTML = "<h3>" + monster.name + "</h3>"
@@ -59,7 +61,7 @@ function attack() {
         + "<h3> HP: " + monster.HP + "</h3>"
         + "<h3> ATK: " + monster.ATK + "</h3>";
     if (player.HP <= 0) {
-        alert("YOU ARE DEAD!")
+        alert("YOU ARE DEAD!");
         localStorage.setItem("Level", 1);
         document.location.href = 'http://127.0.0.1:5000/hero';
     }
